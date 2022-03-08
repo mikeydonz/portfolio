2b318d64411268c5908e6fad1c4bbced92ce5b69
mikeydonz <97123119+mikeydonz@users.noreply.github.com>
2022-03-08 06:02:36 -0800

    Updated script

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,39 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+function randomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
-    document.getElementById('myDropdown').style.display = document.getElementById('myDropdown').style.display =='block' ?   'none' : 'block';
+function randomize() {
+    var rand1 = setInterval(
+        function () {
+          var randomColor = Math.floor(Math.random()*16777215).toString(16);
+          document.getElementById("color1").style.color = "#"+randomColor;
+        },250);
+    
+    var rand2 = setInterval(
+        function () {
+          var randomColor = Math.floor(Math.random()*16777215).toString(16);
+          document.getElementById("color1").style.borderColor = "#"+randomColor;
+          document.getElementById("color1").style["boxShadow"] = "0 0 5px #999999";
+        },250);        
+
+    setTimeout(stop_interval, 5000);
+
+    function stop_interval()
+    {
+      clearInterval(rand1);
+      clearInterval(rand2);
+      document.getElementById("color1").style.color = "black";
+      document.getElementById("color1").style.borderColor = "black";
+      document.getElementById("color1").style["boxShadow"] = "0 0 0px #999999";
+    }
+
 }
+
+var logoHover = document.getElementById("color1");
+
+logoHover.addEventListener('mouseenter', e => {
+    logoHover.style.boxShadow = "-1px 1px 1px black";
+});
+
+logoHover.addEventListener('mouseleave', e => {
+    logoHover.style.boxShadow = "0px 0px 0px black";});
