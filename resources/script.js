a7e3818c4fa274ad86e81365888ce71b7e4508b3
Noahie <97123119+mikeydonz@users.noreply.github.com>
2022-05-03 13:26:08 -0700

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -36,4 +36,43 @@ logoHover.addEventListener('mouseenter', e => {
 });
 
 logoHover.addEventListener('mouseleave', e => {
-    logoHover.style.boxShadow = "0px 0px 0px black";});
+    logoHover.style.boxShadow = "0px 0px 0px black";
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.icon')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myGunction()};
+
+// // Get the header
+// var header = document.getElementById("myHeader");
+
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myGunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
